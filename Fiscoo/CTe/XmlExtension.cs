867e4d3996e8d85084571150909337d838c374a9
Breno Martinusso <martinusso@gmail.com>
2016-10-31 13:20:19 -0200

    Adds Consulta situação atual da CT-e (#2)

---
+++ Fiscoo/CTe/XmlExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Fiscoo.CTe
+{
+    public static class XmlExtension
+    {
+        public static string Serialize<T>(this T value)
+        {
+            if (value == null) return string.Empty;
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, "http://www.portalfiscal.inf.br/cte");
+
+            var settings = new XmlWriterSettings();
+            settings.Encoding = Encoding.UTF8;
+            //settings.Encoding = new UnicodeEncoding(false, false); // no BOM in a .NET string
+            settings.Indent = false;
+            settings.OmitXmlDeclaration = true;
+
+            using (var stringWriter = new StringWriter())
+            {
+                using (var xmlWriter = XmlWriter.Create(stringWriter, settings))
+                {
+                    xmlSerializer.Serialize(xmlWriter, value, namespaces);
+                    return stringWriter.ToString();
+                }
+            }
+        }
+    }
+}
