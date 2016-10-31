6d63da923372fbe61641fc2795ff657bbeac30db
Breno Martinusso <martinusso@gmail.com>
2016-10-31 13:06:25 -0200

    Adds Consulta do Status do Serviço CT-e (#1)

---
+++ Fiscoo.Tests/CTe/ConsStatServCteTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fiscoo.CTe;
+
+namespace Fiscoo.Tests.CTe
+{
+    [TestClass]
+    public class ConsStatServCteTests
+    {
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var expected = @"<consStatServCte versao=""2.00"" xmlns=""http://www.portalfiscal.inf.br/cte""><tpAmb>2</tpAmb><xServ>STATUS</xServ></consStatServCte>";
+            var consStatServCte = new ConsStatServCte(2);
+            Assert.AreEqual(expected, consStatServCte.Serialize());
+        }
+    }
+}
